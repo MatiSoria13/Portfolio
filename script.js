76d988440532131e2a0e2dd6ff44af3cdb65aa79
Mathias Soria <matisoria08@gmail.com>
2024-07-08 23:33:02 -0300

    Portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', () => {
+        navList.classList.toggle('nav-list-visible');
+    });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
